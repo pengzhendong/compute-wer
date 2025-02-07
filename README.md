9d26f3d56855a4aa12f718966096c846beeb7e0f
pengzhendong <275331498@qq.com>
2025-02-07 18:07:38 +0000

    it works

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # compute-wer
+
+## Usage
+
+``` bash
+$ pip install compute-wer
+$ compute-wer ref.txt hyp.txt
+```
--- setup.py
+++ setup.py
@@ -29,6 +29,11 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "compute-wer = compute_wer.cli:main",
+        ]
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
